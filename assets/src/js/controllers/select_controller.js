eb2e72d872f5b61b9d3394b9c3580e37db4a6113
Sébastien Eustace <sebastien@eustace.io>
2021-06-04 18:34:39 +0200

    Add dynamic documentation support

---
+++ assets/src/js/controllers/select_controller.js
@@ -0,0 +1,32 @@
+import { Controller } from 'stimulus'
+import { useClickOutside, useTransition } from 'stimulus-use'
+
+
+export default class extends Controller {
+    static targets = ['button', 'menu']
+    static values = { originStyle: String, targetStyle: String }
+
+    connect() {
+        useTransition(this, { element: this.menuTarget })
+        useClickOutside(this)
+    }
+
+    open() {
+        this.enter()
+    }
+
+    close(event) {
+        if (event.keyCode && event.keyCode != 27) {
+            return
+        }
+        this.leave()
+    }
+
+    toggle() {
+        console.log("Toggle")
+        this.toggleTransition()
+        if (this.transitioned) {
+            // Focus on first element
+        }
+    }
+}
