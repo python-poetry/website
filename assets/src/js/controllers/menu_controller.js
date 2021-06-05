8b994a5117ecc640e949ef86dc2a3a0c43992405
Sébastien Eustace <sebastien@eustace.io>
2021-06-05 13:54:41 +0200

    Use StimulusJS instead of AlpineJS

--- assets/src/js/controllers/menu_controller.js
+++ assets/src/js/controllers/menu_controller.js
@@ -1,19 +1,25 @@
 import { Controller } from 'stimulus'
+import { useClickOutside, useTransition } from 'stimulus-use'
 
 
 export default class extends Controller {
-    static targets = ['menu']
+    static targets = ['button', 'menu']
     static values = { originStyle: String, targetStyle: String }
 
-    show() {
-        this.menuTarget.hidden = false
-        this.menuTarget.classList.remove(...(this.originStyleValue.split(' ')))
-        this.menuTarget.classList.add(...(this.targetStyleValue.split(' ')))
+    connect() {
+        useTransition(this, { element: this.menuTarget })
+        useClickOutside(this, { element: this.menuTarget })
     }
 
-    hide() {
-        this.menuTarget.classList.remove(...(this.targetStyleValue.split(' ')))
-        this.menuTarget.classList.add(...(this.originStyleValue.split(' ')))
-        this.menuTarget.hidden = true
+    open() {
+        this.enter()
+    }
+
+    close(event) {
+        if (event.keyCode && event.keyCode != 27) {
+            return
+        }
+        this.leave()
+        this.buttonTarget.focus()
     }
 }
