0615d334e3ad9be3d07231cd4bccf9fe1e633bfc
Arun Babu Neelicattu <arun.neelicattu@gmail.com>
2021-06-06 21:10:25 +0200

    app: fix application undefined error

--- assets/src/app.js
+++ assets/src/app.js
@@ -5,7 +5,7 @@ import { TransitionController, ClickOutsideController } from 'stimulus-use'
 import MenuController from './js/controllers/menu_controller'
 import SelectController from './js/controllers/select_controller'
 
-application = Application.start()
+const application = Application.start()
 application.register("transition", TransitionController)
 application.register("click-outside", ClickOutsideController)
 application.register("menu", MenuController)
