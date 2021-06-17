178f3eb4a4cce6c153d9a0988261af1d6daba32e
Sébastien Eustace <sebastien@eustace.io>
2021-06-17 14:21:55 +0200

    Add main menu entry for the documentation

---
+++ assets/src/js/controllers/flyover_controller.js
@@ -0,0 +1,19 @@
+import { Controller } from 'stimulus'
+import { useTransition } from 'stimulus-use'
+
+
+export default class extends Controller {
+    static targets = ['button', 'menu']
+
+    connect() {
+        useTransition(this, { element: this.menuTarget })
+    }
+
+    open() {
+        this.enter()
+    }
+
+    close() {
+        this.leave()
+    }
+}
